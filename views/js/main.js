5f4626109aaf65b31b85e5c1e9fd4cebbca52f05
benrobo <alumonabenaiah71@gmail.com>
2021-07-08 13:54:35 +0100

    first commit

--- views/js/main.js
+++ views/js/main.js
@@ -30,3 +30,53 @@ function AnimateImg(){
 
 }
 AnimateImg()
+
+
+function sendMail(){
+    let form = document.getElementById("contact-form");
+    let username = document.querySelector(".username")
+    let useremail = document.querySelector(".useremail")
+    let usermsg = document.querySelector(".usermsg")
+
+    let sendbtn = document.querySelector(".contact-btn")
+
+    sendbtn.onclick = (e)=>{
+        e.preventDefault();
+
+        validate(username.value, useremail.value, usermsg.value)
+    }
+
+}
+sendMail()
+
+function validate(name, mail, msg){
+    if(name == "" || mail == "" || msg == ""){
+        return error()
+    }
+    else{
+        emailjs.send("service_hd5qbz2","template_i2zy4a9",{
+            from_name: name,
+            to_name: "wisdomtechsafetysolutions@gmail.com",
+            message: `
+                ${msg}
+            `,
+        });
+        success()
+    }
+}
+
+const error = ()=>{
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Fielsd cannot be empty!',
+    })
+}
+
+const success = ()=>{
+    Swal.fire({
+        icon: 'success',
+        title: 'Email Sent',
+        text: 'Thanks for contacting us, we get back to you as soon as possible!',
+    })
+}
