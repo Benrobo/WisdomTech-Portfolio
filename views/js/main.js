f88ce3da29b8dea0b70253ffd700cdbb3411fa24
benrobo <alumonabenaiah71@gmail.com>
2021-07-08 10:42:11 +0100

    first commit

---
+++ views/js/main.js
@@ -0,0 +1,33 @@
+function toggleNav(){
+    let navbarbtn = document.querySelector(".btn-toggler");
+    let sidebar = document.querySelector(".sidebar");
+
+    navbarbtn.onclick = ()=>{
+        sidebar.classList.toggle("slide")
+    }
+}
+toggleNav()
+
+
+$(document).ready(function(){
+    $('.project-img-cont').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery:{
+            enabled:true
+        }
+        // other options
+    });
+})
+
+function AnimateImg(){
+    let pic = document.querySelectorAll(".pic");
+    pic.forEach((el)=>{
+        let img = document.querySelectorAll("img")
+        img.forEach((imge)=>{
+            imge.setAttribute("data-aos", "fade-up")
+        })
+    })
+
+}
+AnimateImg()
